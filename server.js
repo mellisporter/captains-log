68ec373be6563adbe912dd0e6b9b180a9d85a66f
Matt Porter <matthew.ellis.porter@gmail.com>
2022-09-06 14:17:32 -0400

    create route works

--- server.js
+++ server.js
@@ -3,10 +3,31 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+require('dotenv').config();
+
+
+const mongoose = require('mongoose');
+
 
 // DATABASE
 
 // MIDDLEWARE
+
+// Database Connection process.env is how we acess the variable from the env file
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+// Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// Define callback functions for various events
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 // Index
 
@@ -21,6 +42,18 @@ app.get("/logs/new" , function (req, res){
 // Update
 
 // Create
+
+app.post("/logs" , function (req, res){
+    // res.send('received')
+    if (req.body.shipIsBroken === 'on') {
+		//if checked, req.body.completed is set to 'on'
+		req.body.shipIsBroken = true;
+	} else {
+		//if not checked, req.body.completed is undefined
+		req.body.shipIsBroken = false;
+	}
+    res.send(req.body)
+})
 
 // Edit
 
